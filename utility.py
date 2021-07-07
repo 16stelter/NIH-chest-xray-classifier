d93ceafbd8488baf3a5c9d3b5cf87405c1248a28
Sebastian Stelter <6stelter@informatik.uni-hamburg.de>
2021-07-07 17:10:41 +0200

    Added label to image matching

--- utility.py
+++ utility.py
@@ -9,15 +9,23 @@ class Utility:
         split_ind = int(0.9 * len(self._filenames))
         self._training_names, self._test_names = self._filenames[:split_ind], self._filenames[split_ind:]
         print("I found %d training and %d test .tfrecord files." % (len(self._training_names), len(self._test_names)))
+        self._df = pd.read_csv(path + "/preprocessed_data.csv")
 
     def read_tfrecord(self, sample):
         tfrecord_format = {
                 "image": tf.io.FixedLenFeature([], tf.string),
             }
+        for e in list(self._df.columns)[1:]:
+            tfrecord_format[e] = tf.io.FixedLenFeature([], tf.int64)
 
         sample = tf.io.parse_single_example(sample, tfrecord_format)
-        sample = self.decode_image(sample["image"])
-        return sample
+        image = self.decode_image(sample["image"])
+
+        label = []
+        for i in range(len(self._df.columns[1:])):
+            label.append(sample[self._df.columns[i+1]])
+
+        return image, label
 
     def decode_image(self, image):
         image = tf.image.decode_jpeg(image, channels=3)
@@ -32,14 +40,13 @@ class Utility:
         ds = ds.with_options(
             ignore_order
         )
-        plt.imshow(next(iter(ds)) / 255.0)
+
+        ex = next(iter(ds))
+        print(ex[1:])
+        plt.imshow(ex[0] / 255.0)
         plt.show()
 
-    def load_csv(self, file):
-        df = pd.read_csv(file)
-        return df
 
 
 ut = Utility(".")
-print(ut.load_csv("./preprocessed_data.csv")["Unnamed: 0"][0])
 ut.create_dataset(ut._training_names[0])
