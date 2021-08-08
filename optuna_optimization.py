fecddccff3e985b17d828ee9e8d60707429108d4
Sebastian Stelter <6stelter@informatik.uni-hamburg.de>
2021-08-08 13:09:39 +0200

    optuna fixes

--- optuna_optimization.py
+++ optuna_optimization.py
@@ -32,14 +32,12 @@ class Objective(object):
                                       input_shape=(100, 100, 3)))
         self._model.add(layers.Conv2D(dict_params['n_filters'],
                                       (dict_params['kernel_size'], dict_params['kernel_size']),
-                                      activation='relu',
-                                      input_shape=(100, 100, 3)))
+                                      activation='relu'))
         self._model.add(layers.MaxPooling2D((2, 2)))
         for i in range(dict_params['n_additional_layers']):
             self._model.add(layers.Conv2D(dict_params['n_filters'],
                                           (dict_params['kernel_size'], dict_params['kernel_size']),
-                                          activation='relu',
-                                          input_shape=(100, 100, 3)))
+                                          activation='relu'))
             if i+1 % 2 == 0:
                 self._model.add(layers.MaxPooling2D((2, 2)))
 
@@ -48,16 +46,22 @@ class Objective(object):
         self._model.add(layers.Dense(15, activation='sigmoid'))
 
         opt = keras.optimizers.Adam(lr=dict_params['learning_rate'])
-        self._model.compile(optimizer=opt, loss=self.loss, metrics=[tf.keras.metrics.AUC(name="auc")])
+        self._model.compile(optimizer=opt, loss="binary_crossentropy", metrics=[tf.keras.metrics.AUC(name="auc")])
 
         filepath = "./weights"
         checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='max',
                                      save_freq='epoch')
         print(tf.data.experimental.cardinality(self.ds).numpy())
         print(dict_params)
+        class_weights = [0.1237545, 0.63923679, 2.22684278, 0.47803896, 2.06822319, 6.58236776,
+                         6.04907407, 7.05317139, 1.31780131, 20.02452107, 5.08899708, 6.51670823,
+                         2.39743119, 3.55537415, 68.76842105]
+
+        class_weights = {i : class_weights[i] for i in range(len(class_weights))}
         self._history = self._model.fit(self.ds, epochs=2,
-                                        steps_per_epoch=tf.data.experimental.cardinality(self.ds).numpy(),
-                                        callbacks=[checkpoint])
+                                        steps_per_epoch=200,
+                                        callbacks=[checkpoint],
+                                        class_weight=class_weights)
 
         loss = np.min(self._history.history['loss'])
         return loss
@@ -71,13 +75,12 @@ early_stop = 10
 lr_epochs = 5
 opt_direction = 'minimize'
 n_random = 25
-max_time = 6*60*60
+max_time = 5*60*60
 
 _ut = utility.Utility(".")
-#cal_ds = _ut.create_balanced_dataset(_ut.get_training_names(), 110)
 cal_ds = tf.data.experimental.load("./cal_ds", (tf.TensorSpec(shape=(None, 100, 100, 3), dtype=tf.float32, name=None), tf.TensorSpec(shape=(None, 15), dtype=tf.int64, name=None))).repeat(1000)
 
-objective = Objective(cal_ds)
+objective = Objective(_ut.create_dataset(_ut.get_training_names()).repeat(1000))
 
 study = optuna.create_study(direction=opt_direction, sampler=TPESampler(n_startup_trials=n_random))
 
